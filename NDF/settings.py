4fdaec6b7ab2167658905dbffd26858c25ebe6c6
elpadrino94 <celestindjato5@gmail.com>
2025-10-08 09:21:50 +0000

    variable d'environnement

--- NDF/settings.py
+++ NDF/settings.py
@@ -1,6 +1,7 @@
 
 
 import os
+from decouple import config
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -11,7 +12,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-yf6)w-2^!&meliat#)-u+_7lax&k17)_%r(49jnuxh46)@y^!1'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
